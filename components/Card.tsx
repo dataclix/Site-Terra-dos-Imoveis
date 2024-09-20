56ef6e74d60dc2c261f06a8945f7c2b8570970b8
rafael-campos <32342615+rafael-campos@users.noreply.github.com>
2024-09-20 13:06:28 -0300

    ajustes finos

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import {
-    FaHeart,
-    FaBed,
-    FaBath,
-    FaCar,
-    FaRulerCombined,
-    FaShareAlt,
-    FaWhatsapp, // Re-adicionado FaWhatsapp 
-    FaCopy,
-    FaFacebook,
-} from 'react-icons/fa';
+    HiOutlineHome,
+    HiOutlineTruck,
+} from 'react-icons/hi';
+import {
+    FiHeart,
+    FiCopy,
+    FiFacebook,
+    FiSend,
+    FiShare, // Novo ícone para compartilhar
+} from 'react-icons/fi'; 
+import { BiBath } from 'react-icons/bi'; // Novo ícone para banheiro
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -23,6 +24,8 @@ import {
 } from 'react-share';
 
 import { urlSite } from './globals/variavels';
+
+import { RiRulerLine } from 'react-icons/ri';
 
 export interface ImovelResumo {
     id: string;
@@ -61,21 +64,17 @@ const Card: React.FC<ImovelResumo> = ({
     id,
     idInterno,
 
-    captador,
     tipo,
     subtipo,
-
     categoria,
     bairro,
     cidade,
-
     estadoSigla,
     valor,
     quartos,
     banheiros,
     garagens,
     areaConstruida,
-
     modalidade,
     fotos,
 }) => {
@@ -109,29 +108,8 @@ const Card: React.FC<ImovelResumo> = ({
         window.dispatchEvent(event);
     };
 
-    function cleanPhoneNumber(phoneNumber: string) {
-        const cleanedNumber = phoneNumber.replace(/[^\d]/g, '');
-        return `55${cleanedNumber}`;
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars 
-    const handleWhatsapp = () => { // Comentário para desabilitar a regra nesta linha
-        let numeroWhatsapp = '5532988898000';
-        if (modalidade === 'ALUGUEL') {
-            numeroWhatsapp = '5532988118000';
-        }
-        if (captador.celular && captador.celular !== '' && captador.creci && captador.creci !== '') {
-            numeroWhatsapp = cleanPhoneNumber(captador.celular);
-        }
-        const mensagem = encodeURIComponent(
-            `Olá! Tenho interesse no imóvel ${idInterno}!`
-        );
-        const shareUrl = `https://api.whatsapp.com/send?phone=${numeroWhatsapp}&text=${mensagem}`;
-        window.open(shareUrl, '_blank');
-    };
-
     const [showMenu, setShowMenu] = useState(false);
-    const shareUrl = 'https://seusite.com'; //ajuste a url
+    const shareUrl = 'https://seusite.com'; // ajuste a URL
 
     const handleCopyLink = () => {
         navigator.clipboard.writeText(shareUrl).then(() => {
@@ -139,142 +117,127 @@ const Card: React.FC<ImovelResumo> = ({
             setShowMenu(false);
         });
     };
+
     const toggleMenu = () => {
         setShowMenu(!showMenu);
     };
+
     const subtipoText = subtipo === 'Padrão' ? '' : ` - ${subtipo}`;
     const cidadeEstadoText = cidade ? `${cidade} - ${estadoSigla}` : estadoSigla;
 
     return (
-        <div className="bg-white shadow-sm rounded-lg overflow-hidden cursor-auto transform transition duration-300 hover:scale-105 relative">
-            <div className="">
-                <Link href={'/' + idInterno}>
-                    <div className="relative z-5 px-[4%]" >
-
-                        <Swiper id='custom' navigation pagination={{ clickable: true }} modules={[Navigation, Pagination]} className="w-full xl:h-80 h-[360px]  mb-4">
-                            {fotos.length > 0 ? fotos.map((foto, index) => (
+        <div className="bg-white shadow-sm rounded-lg overflow-hidden cursor-pointer transform transition duration-300 hover:scale-105 relative">
+            <Link href={'/' + idInterno}>
+                <div className="relative z-5 px-4">
+                    {/* Swiper de imagens */}
+                    <Swiper navigation pagination={{ clickable: true }} modules={[Navigation, Pagination]} className="w-full h-60 mb-4">
+                        {fotos.length > 0 ? (
+                            fotos.map((foto, index) => (
                                 <SwiperSlide key={index}>
-                                    <img width={600} height={320} src={foto} alt={`Foto ${index + 1} do imóvel`} className="w-full xl:block hidden xl:h-80 h-[320px] object-cover rounded-xl" />
-                                    <img width={600} height={360} src={foto} alt={`Foto ${index + 1} do imóvel`} className="w-full xl:hidden block xl:h-80 h-[360px] object-cover rounded-xl" />
+                                    <img
+                                        width={600}
+                                        height={320}
+                                        src={foto}
+                                        alt={`Foto ${index + 1} do imóvel`}
+                                        className="w-full h-full object-cover rounded-md"
+                                    />
                                 </SwiperSlide>
-                            )) :
-                                <SwiperSlide key={'0'}>
-                                    <div className='flex justify-center items-center xl:h-80 h-[360px] '>
-                                        <p>Sem Foto</p>
-                                    </div>
-                                </SwiperSlide>
-
-                            }
-                        </Swiper>
-
-                        {/* Modalidade em cima da imagem */}
-                        {categoria && (
-                            <div className={`absolute z-20 top-2 left-4 px-3 py-1 rounded-full text-[10px] text-black bg-opacity-90 font-semibold bg-slate-100 `} >
-                                {modalidade}
-                            </div>
-                        )}
-
-
-
-                    </div>
-
-                    {/* Informações do Imóvel */}
-                    <div className="px-8 pb-4">
-                        <div className='flex justify-between items-center mb-2'>
-                            <p className='xl:text-lg text-md  text-[#52174c] font-bold '>{bairro}</p>
-
-
-                            <div className="flex items-center space-x-2">
-                                <span className="text-gray-600 text-md">COD {idInterno}</span>
-
-
-                            </div>
-                        </div>
-
-                        {/* Ícones dos Detalhes do Imóvel */}
-                        <div className="flex flex-wrap mt-4 text-gray-600 gap-x-8 xl:h-6 text-sm mb-4">
-                            {quartos && quartos !== 0 && (
-                                <span className="flex items-center">
-                                    <FaBed className="mr-1 text-gray-500" size={20} />{' '}
-                                    {quartos}
-                                </span>
-                            )}
-                            {banheiros && banheiros !== 0 && (
-                                <span className="flex items-center">
-                                    <FaBath className="mr-1 text-gray-500" size={20} />{' '}
-                                    {banheiros}
-                                </span>
-                            )}
-                            {garagens && garagens !== 0 && (
-                                <span className="flex items-center">
-                                    <FaCar className="mr-1 text-gray-500" size={20} />{' '}
-                                    {garagens}
-                                </span>
-                            )}
-                            {areaConstruida && areaConstruida !== '0' && areaConstruida !== '' && (
-                                <span className="flex items-center">
-                                    <FaRulerCombined className="mr-1 text-gray-500" size={20} />{' '}
-                                    {areaConstruida} m²
-                                </span>
-                            )}
-                        </div>
-
-                        {<p className="xl:text-md text-sm mb-2 font-bold">
-                            <span className="">{tipo} {subtipoText}</span>
-                        </p>}
-
-
-                        <p className="text-md  text-gray-800">{cidadeEstadoText}</p>
-
-
-
-                        {/* Valor do Imóvel e Botão "Veja Mais" */}
-                        <div className="flex justify-between items-center ">
-                            <p className="text-lg xl:text-xl font-bold text-[#52174c] ">
-                                {valor.toLocaleString('pt-BR', {
-                                    style: 'currency',
-                                    currency: 'BRL',
-                                    minimumFractionDigits: 0,
-                                    maximumFractionDigits: 0,
-                                })},<span className='text-sm'>00</span>
-                            </p>
-                            <button onClick={toggleMenu} className="p-2 rounded-full text-[#52174c] ">
-                                <FaShareAlt size={24} />
-                                <p className='text-[10px] w-0 text-transparent'>Compartilhar</p>
-                            </button>
-
-                            {showMenu && (
-                                <div className="absolute z-40 top-12 right-0 bg-white shadow-lg p-4 rounded-md mt-2">
-                                    <h3 className="mb-2">Compartilhar em:</h3>
-                                    <div className="flex flex-col space-y-2">
-                                        <WhatsappShareButton url={urlSite + idInterno} onClick={() => setShowMenu(false)}>
-                                            <button className="w-full p-2 hover:bg-gray-200 rounded-md text-green-600 flex items-center">
-                                                <FaWhatsapp className="mr-2" />
-                                                WhatsApp
-                                            </button>
-                                        </WhatsappShareButton>
-                                        <FacebookShareButton url={urlSite + idInterno} onClick={() => setShowMenu(false)}>
-                                            <button className="w-full p-2 hover:bg-gray-200 rounded-md text-blue-600 flex items-center">
-                                                <FaFacebook className="mr-2" />
-                                                Facebook
-                                            </button>
-                                        </FacebookShareButton>
-                                        <button onClick={handleCopyLink} className="w-full p-2 hover:bg-gray-200 rounded-md text-gray-600 flex items-center">
-                                            <FaCopy className="mr-2" />
-                                            Copiar link
-                                        </button>
-                                    </div>
+                            ))
+                        ) : (
+                            <SwiperSlide key="0">
+                                <div className="flex justify-center items-center h-full">
+                                    <p>Sem Foto</p>
                                 </div>
-                            )}
+                            </SwiperSlide>
+                        )}
+                    </Swiper>
 
-                            <button onClick={handleToggleFavorite} className={`p-2 rounded-full ${isFavorite ? 'text-[#f4505a]' : 'text-[#52174c] '}`}>
-                                <FaHeart size={24} />
-                                <p className='text-[10px] w-0 text-transparent'>Favorito</p>
+                    {/* Modalidade em cima da imagem */}
+                    {categoria && (
+                        <div className="absolute top-2 left-4 px-3 py-1 rounded-full text-xs font-semibold bg-gray-100 bg-opacity-90">
+                            {modalidade}
+                        </div>
+                    )}
+                </div>
+
+                {/* Informações do imóvel */}
+                <div className="px-6 pb-4">
+                    <div className="flex justify-between items-center mb-2">
+                        <p className="text-lg text-[#52174c] font-bold">{bairro}</p>
+                        <span className="text-gray-500 text-sm">COD {idInterno}</span>
+                    </div>
+
+                    {/* Ícones dos Detalhes do Imóvel */}
+                    <div className="flex flex-wrap text-gray-600 gap-x-6 gap-y-2 mb-4 text-lg">
+                        {quartos && (
+                            <span className="flex items-center">
+                                <HiOutlineHome className="mr-1 text-gray-500" size={18} /> {quartos}
+                            </span>
+                        )}
+                        {banheiros && (
+                            <span className="flex items-center">
+                                <BiBath className="mr-1 text-gray-500" size={18} /> {banheiros} {/* Novo ícone de banheiro */}
+                            </span>
+                        )}
+                        {garagens && (
+                            <span className="flex items-center">
+                                <HiOutlineTruck className="mr-1 text-gray-500" size={18} /> {garagens}
+                            </span>
+                        )}
+                        {areaConstruida && (
+                            <span className="flex items-center">
+                                <RiRulerLine className="mr-1 text-gray-500" size={18} /> {areaConstruida} m²
+                            </span>
+                        )}
+                    </div>
+
+                    {/* Informações do imóvel */}
+                    <p className="text-sm font-bold">{tipo} {subtipoText}</p>
+                    <p className="text-sm text-gray-500">{cidadeEstadoText}</p>
+
+                    {/* Valor do Imóvel e Botões de ação */}
+                    <div className="flex justify-between items-center mt-4">
+                        <p className="text-lg font-bold text-[#52174c]">
+                            {valor.toLocaleString('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL',
+                                minimumFractionDigits: 0,
+                            })},00
+                        </p>
+
+                        <div className="flex items-center space-x-4">
+                            <button onClick={toggleMenu} className="p-2 rounded-full text-[#52174c]">
+                                <FiShare className="mr-1" size={20} /> {/* Novo ícone de compartilhar */}
+                            </button>
+
+                            <button onClick={handleToggleFavorite} className={`p-2 rounded-full ${isFavorite ? 'text-red-600' : 'text-[#52174c]'}`}>
+                                <FiHeart size={20} />
                             </button>
                         </div>
                     </div>
-                </Link>
-            </div>
+
+                    {showMenu && (
+                        <div className="absolute z-40 top-12 right-0 bg-white shadow-lg p-4 rounded-md mt-2">
+                            <h3 className="mb-2 text-sm">Compartilhar em:</h3>
+                            <div className="flex flex-col space-y-2">
+                                <WhatsappShareButton url={urlSite + idInterno} onClick={() => setShowMenu(false)}>
+                                    <button className="w-full p-2 hover:bg-gray-100 rounded-md text-green-600 flex items-center">
+                                        <FiSend className="mr-2" /> WhatsApp
+                                    </button>
+                                </WhatsappShareButton>
+                                <FacebookShareButton url={urlSite + idInterno} onClick={() => setShowMenu(false)}>
+                                    <button className="w-full p-2 hover:bg-gray-100 rounded-md text-blue-600 flex items-center">
+                                        <FiFacebook className="mr-2" /> Facebook
+                                    </button>
+                                </FacebookShareButton>
+                                <button onClick={handleCopyLink} className="w-full p-2 hover:bg-gray-100 rounded-md text-gray-600 flex items-center">
+                                    <FiCopy className="mr-2" /> Copiar link
+                                </button>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </Link>
         </div>
     );
 };
